17abca707529ee43025c8221792bd6cfff864c13
Kate <cimonyu@mail.ru>
2024-07-25 18:18:41 +0300

    рассчет месячных продаж по представленому массиву

---
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -0,0 +1,65 @@
+package ru.netology.stats;
+
+public class StatsService {
+
+    public long sumSales(long[] sales) {
+        long totalSale = 0;
+        for (long sale : sales) {
+            totalSale += sale;
+        }
+        return totalSale;
+
+    }
+
+    public long averageSales(long[] sales) {
+        return sumSales(sales) / sales.length;
+    }
+
+    public long calcMonthMaxSale(long[] sales) {
+        int monthMaxSale = 0;
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] >= sales[monthMaxSale]) {
+                monthMaxSale = i;
+
+            }
+        }
+        return monthMaxSale + 1;
+    }
+
+    public long calcMonthMinSale(long[] sales) {
+        int monthMinSale = 0;
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] <= sales[monthMinSale]) {
+                monthMinSale = i;
+
+            }
+        }
+        return monthMinSale + 1;
+    }
+
+    public int salesMonthsBelowAverage (long[] sales) {
+        int counter = 0;
+        long average = averageSales(sales);
+        for (long sale : sales) {
+            if ( sale < average) {
+                counter++;
+            }
+        }
+
+        return counter;
+
+    }
+    public int salesMonthsAboveAverage (long[] sales) {
+        int counter = 0;
+        long average = averageSales(sales);
+        for (long sale : sales) {
+            if ( sale > average) {
+                counter++;
+            }
+        }
+
+        return counter;
+    }
+
+}
+
---
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -0,0 +1,65 @@
+package ru.netology.stats;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatsServiceTest {
+
+    @Test
+    public void testSum() {
+        StatsService statsService = new StatsService();
+        long [] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long actual = statsService.sumSales(sales);
+        long expected = 8 + 15 + 13 + 15 + 17 + 20 + 19 + 20 + 7 + 14 + 14 + 18;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testAverage() {
+        StatsService statsService = new StatsService();
+        long [] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long actual = statsService.averageSales(sales);
+        long expected = ( 8 + 15 + 13 + 15 + 17 + 20 + 19 + 20 + 7 + 14 + 14 + 18) / 12;
+        Assertions.assertEquals(expected, actual);
+}
+    @Test
+    public void testMonthMaximum() {
+        StatsService statsService = new StatsService();
+        long [] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long actual = statsService.calcMonthMaxSale(sales);
+        long expected = 8;
+        Assertions.assertEquals(expected, actual);
+
+    }
+    @Test
+    public void testMonthMinimum() {
+        StatsService statsService = new StatsService();
+        long [] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long actual = statsService.calcMonthMinSale(sales);
+        long expected = 9;
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSalesMonthsBelowAverage() {
+        StatsService statsService = new StatsService();
+        long [] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long actual = statsService.salesMonthsBelowAverage(sales);
+        long expected = 5;
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSalesMonthsAboveAverage() {
+        StatsService statsService = new StatsService();
+        long [] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long actual = statsService.salesMonthsAboveAverage(sales);
+        long expected = 5;
+        Assertions.assertEquals(expected, actual);
+    }
+
+}
+
